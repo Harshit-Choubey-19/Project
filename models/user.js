212bb0f4f52bd05fcb3932df81e788c7235571c7
Harshit Choubey <144536334+Harshit-Choubey-19@users.noreply.github.com>
2024-02-27 12:57:35 +0530

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,22 +1,25 @@
 const passport = require("passport");
 require("dotenv").config();
 
-const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
-passport.use(new GoogleStrategy({
-    clientID:     process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/google/callback",
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "https://harshit-choubey-project.onrender.com/",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
       done(null, profile);
-  }
-));
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
-    done(null,user);
+  done(null, user);
 });
 passport.deserializeUser((user, done) => {
-    done(null,user);
+  done(null, user);
 });
