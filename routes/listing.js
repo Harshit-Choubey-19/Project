b93f16eb91e08b91444c095843e3118643ca8b60
Harshit Choubey <144536334+Harshit-Choubey-19@users.noreply.github.com>
2024-04-17 18:53:49 +0530

    Add files via upload

--- routes/listing.js
+++ routes/listing.js
@@ -6,74 +6,170 @@ const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn } = require("../middleware.js");
 
-const validateListing = (req,res,next) => {
-    let { error } = listingSchema.validate(req.body);
-    if(error) {
-      let errMsg = error.details.map((el) => el.message).join(",");
-      throw new ExpressError(400, errMsg);
-    }else {
-      next();
-    }
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
   }
+};
 
-  //Index Route
-  router.get("/", wrapAsync(async (req, res) => { 
+//Index Route
+router.get(
+  "/",
+  wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-  }));
-  
-  //New Route
-  router.get("/new", isLoggedIn, (req, res) => {
-    res.render("listings/new.ejs");
-  });
-  
-  //Show Route
-  router.get("/:id", wrapAsync(async (req, res) => {
+  })
+);
+
+//New Route
+router.get("/new", isLoggedIn, (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//search
+router.get(
+  "/search",
+  wrapAsync(async (req, res) => {
+    let input = req.query.q.trim().replace(/\s+/g, " "); //remove start and end space
+    if (input == "" || input == " ") {
+      //search value is empty
+      req.flash("error", "Search value empty!!!");
+      res.redirect("/listings");
+    }
+    //convert every word first letter capital and other small
+    let data = input.split("");
+    let element = "";
+    let flag = false;
+    for (let index = 0; index < data.length; index++) {
+      if (index == 0 || flag) {
+        element = element + data[index].toUpperCase();
+      } else {
+        element = element + data[index].toLowerCase();
+      }
+      flag = data[index] == " ";
+    }
+    console.log(element);
+
+    let allListings = await Listing.find({
+      title: { $regex: element, $options: "i" },
+    });
+    if (allListings.length != 0) {
+      res.locals.success = "Listings searched by title";
+      res.render("listings/index.ejs", { allListings });
+      return;
+    }
+    if (allListings.length == 0) {
+      allListings = await Listing.find({
+        country: { $regex: element, $options: "i" },
+      }).sort({ _id: -1 });
+      if (allListings.length != 0) {
+        res.locals.success = "Listings searched by country";
+        res.render("listings/index.ejs", { allListings });
+        return;
+      }
+    }
+    if (allListings.length == 0) {
+      allListings = await Listing.find({
+        location: { $regex: element, $options: "i" },
+      }).sort({ _id: -1 });
+      if (allListings.length != 0) {
+        res.locals.success = "Listings searched by location";
+        res.render("listings/index.ejs", { allListings });
+        return;
+      }
+    }
+    const intValue = parseInt(element, 10); //10 for decimal return - int ya NaN
+    const intDec = Number.isInteger(intValue); //check intValue is number or not
+
+    if (allListings.length == 0 && intDec) {
+      allListings = await Listing.find({ price: { $lte: element } }).sort({
+        price: 1,
+      });
+      if (allListings.length != 0) {
+        res.locals.success = `Listings searched for less than Rs ${element}`;
+        res.render("listings/index.ejs", { allListings });
+        return;
+      }
+    }
+
+    if (allListings.length == 0) {
+      req.flash("error", "Listings is not here !!!");
+      res.redirect("/listings");
+    }
+  })
+);
+
+//Show Route
+router.get(
+  "/:id",
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-    if(!listing) {
+    if (!listing) {
       req.flash("error", "Listing you requested for doesn't exist!");
       res.redirect("/listings");
     }
     res.render("listings/show.ejs", { listing });
-  }));
-  
-  //Create Route
-  router.post("/",isLoggedIn, validateListing, wrapAsync(async (req, res, next) => {
-      const newListing = new Listing(req.body.listing);
-      await newListing.save();
-      req.flash("success", "New Listing created!");
-      res.redirect("/listings");
-  }));
-  
-  //Edit Route
-  router.get("/:id/edit", isLoggedIn, wrapAsync(async (req, res) => {
+  })
+);
+
+//Create Route
+router.post(
+  "/",
+  isLoggedIn,
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    req.flash("success", "New Listing created!");
+    res.redirect("/listings");
+  })
+);
+
+//Edit Route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-    if(!listing) {
+    if (!listing) {
       req.flash("error", "Listing you requested for doesn't exist!");
       res.redirect("/listings");
     }
     res.render("listings/edit.ejs", { listing });
-  }));
-  
-  //Update Route
-  router.put("/:id", isLoggedIn, wrapAsync(async (req, res) => {
-    if(!req.body.listing) {
-      throw new ExpressError(400 , "Enter valid data for Listing");
+  })
+);
+
+//Update Route
+router.put(
+  "/:id",
+  isLoggedIn,
+  wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "Enter valid data for Listing");
     }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
-  }));
-  
-  //Delete Route
-  router.delete("/:id",isLoggedIn, wrapAsync(async (req, res) => {
+  })
+);
+
+//Delete Route
+router.delete(
+  "/:id",
+  isLoggedIn,
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
-  }));
+  })
+);
 
-  module.exports = router;
+module.exports = router;
--- schema.js
+++ schema.js
@@ -1,12 +1,12 @@
 const Joi = require("joi");
 
-module.exports.listingSchema = Joi.object( {
-    listing: Joi.object( {
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        location: Joi.string().required(),
-        country: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string().allow("", null),
-    }).required(),
+module.exports.listingSchema = Joi.object({
+  listing: Joi.object({
+    title: Joi.string().required().min(10).max(20),
+    description: Joi.string().required().max(100),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.string().allow("", null),
+  }).required(),
 });
